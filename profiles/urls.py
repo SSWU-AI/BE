dd2d930d74014a023cc741453882bbcbed4b139e
kseohyuz <seohyun518612@naver.com>
2025-05-20 00:47:42 +0900

    Feat: 사용자 본인 프로필 조회 및 수정 API 추가

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,8 +3,12 @@ from rest_framework.routers import DefaultRouter
 from .views import ProfileViewSet
 
 router = DefaultRouter()
-router.register(r'profiles', ProfileViewSet, basename='profile')
+# router.register(r'profiles', ProfileViewSet, basename='profile')  ← 생략해도 됨
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('profiles/me/', ProfileViewSet.as_view({
+        'get': 'retrieve_own_profile',
+        'put': 'update_own_profile',
+        'patch': 'update_own_profile',
+    }), name='profile-me'),
 ]
