ccae8cc373507e8d3fb30a75bf99808697b884b5
kseohyuz <seohyun518612@naver.com>
2025-05-20 00:05:31 +0900

    Feat: 사용자 프로필 모델 회원가입 시 자동 생성 기능 추가

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+class ProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # 본인 프로필만 조회 가능
+        return Profile.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
