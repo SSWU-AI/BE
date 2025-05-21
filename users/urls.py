7e1e41bbf24da263988feaf0a81ca586c5e8aae0
kseohyuz <seohyun518612@naver.com>
2025-05-21 09:57:59 +0900

    Feat: JWT 로그아웃 기능 추가

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SignupView, LoginView, UserInfoView
+from .views import SignupView, LoginView, LogoutView, UserInfoView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('me/', UserInfoView.as_view(), name='user-info'),
 ]
