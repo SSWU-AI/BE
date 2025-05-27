986ca91aac9c24db4098c65b09863914926a1805
kseohyuz <seohyun518612@naver.com>
2025-05-27 16:16:10 +0900

    Fix: swagger 설정

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,12 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import ProfileViewSet
 
 router = DefaultRouter()
-# router.register(r'profiles', ProfileViewSet, basename='profile')  ← 생략해도 됨
+router.register(r'profiles', ProfileViewSet, basename='profile')
 
 urlpatterns = [
-    path('profiles/me/', ProfileViewSet.as_view({
-        'get': 'retrieve_own_profile',
-        'put': 'update_own_profile',
-        'patch': 'update_own_profile',
-    }), name='profile-me'),
+    path('', include(router.urls)),
 ]
